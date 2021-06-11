5e9310f01485bd1d777643ae83aff8f90ca57ec1
Chris Liu <cliu158@ucsc.edu>
2021-06-10 17:05:47 -0700

    added non-tabulated scheduling algorithm.

--- util/scheduler/sched.py
+++ util/scheduler/sched.py
@@ -4,12 +4,16 @@ import yaml
 
 import scheduler
 
+from typing import *
+
 def main():
     parser = create_parser()
     args = parser.parse_args()
-    print(args.profiles)
+    profiles = load_profiles(args.profiles)
+    scheduler.Scheduler(profiles)
 
 def create_parser() -> argparse.ArgumentParser:
+    """Returns a valid python argument parser."""
     parser = argparse.ArgumentParser(description="Simulate and generate " \
                                      "a heterogeneous configuration of " \
                                      "kernels and graph segments.")
@@ -25,6 +29,14 @@ def create_parser() -> argparse.ArgumentParser:
     parser.add_argument('profiles', type=valid_yaml_file, nargs='+',
                         help="YAML benchmark profiles for devices")
     return parser
+
+def load_profiles(fnames: List[str]) -> List[dict]:
+    """Returns contents of profile files."""
+    profiles = list()
+    for fname in fnames:
+        with open(fname, 'r') as ifs:
+            profiles.append(yaml.full_load(ifs))
+    return profiles
 
 if __name__ == '__main__':
     main()
--- util/scheduler/scheduler/scheduler.py
+++ util/scheduler/scheduler/scheduler.py
@@ -1,2 +1,71 @@
-def print_something():
-    print("Something")
+import copy
+from typing import *
+
+class Scheduler:
+    def __init__(self, profiles: List[dict]):
+        self.profiles = profiles
+        print(profiles)
+
+##
+
+#def schedule(profile1, profile2, profile3, seg=0, exec1=0, exec2=0, exec3=0):
+    ### Base case.
+    ### len(profile1) == len(profile2) == len(profile3)
+    #if seg == len(profile1):
+        #return (max(exec1, exec2, exec3), (exec1, exec2, exec3))
+
+    #min_profile = (float('inf'), (0, 0, 0))
+    
+    #(min_exec, execs) = schedule(profile1, profile2, profile3, seg + 1, 
+                                 #exec1 + profile1[seg],
+                                 #exec2,
+                                 #exec3)
+    #if min_exec < min_profile[0]:
+        #min_profile = (min_exec, execs)
+
+    #(min_exec, execs) = schedule(profile1, profile2, profile3, seg + 1, 
+                                 #exec1,
+                                 #exec2 + profile2[seg],
+                                 #exec3)
+    #if min_exec < min_profile[0]:
+        #min_profile = (min_exec, execs)
+
+    #(min_exec, execs) = schedule(profile1, profile2, profile3, seg + 1, 
+                                 #exec1,
+                                 #exec2,
+                                 #exec3 + profile3[seg])
+    #if min_exec < min_profile[0]:
+        #min_profile = (min_exec, execs)
+
+    #return min_profile
+
+def schedule(profiles, execs, segments, seg=0):
+    """Returns the best configuration where the maximum time between all devices
+    is minimized."""
+    # Base case.
+    # len(profiles[0]) == len(profiles[1]) == ... == len(profiles[n - 1])
+    if seg == len(profiles[0]):
+        return (max(*execs), copy.copy(execs), copy.deepcopy(segments))
+
+    ## Recursive case.
+    min_profile = (float('inf'), [], [])
+
+    for i, profile in enumerate(profiles):
+        execs[i] += profile[seg] # What happens if we give device i this segment.
+        segments[i].append(seg)
+        min_res = schedule(profiles, execs, segments, seg + 1)
+        execs[i] -= profile[seg] # Restore original execution times.
+        segments[i].pop()
+        
+        if min_res[0] < min_profile[0]:
+            min_profile = min_res
+
+    return min_profile
+            
+profile1 = [10, 30, 15, 17]
+profile2 = [30, 20, 10, 50]
+profile3 = [25, 5, 30, 25]
+profiles = [profile1, profile2, profile3]
+init_execs = [0] * 3 # Initial execution times.
+init_segments = [[] for _ in range(3)]
+print(schedule(profiles, init_execs, init_segments))
